728ed3d59fe663510e0493e10fcbafc9d7ca6931
prasad <prasad@lvi.co.jp>
2018-06-19 17:04:46 +0900

    test

--- src/main/java/jenkins/plugins/mattermost/workflow/MattermostSendStep.java
+++ src/main/java/jenkins/plugins/mattermost/workflow/MattermostSendStep.java
@@ -3,7 +3,9 @@ package jenkins.plugins.mattermost.workflow;
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.Util;
-import hudson.model.TaskListener;
+import hudson.model.*;
+import hudson.tasks.Publisher;
+import hudson.util.RunList;
 import jenkins.model.Jenkins;
 import jenkins.plugins.mattermost.*;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
@@ -15,6 +17,9 @@ import org.kohsuke.stapler.DataBoundSetter;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Workflow step to send a Slack channel notification.
@@ -125,22 +130,55 @@ public class MattermostSendStep extends AbstractStepImpl {
                 listener.error(String.format("Mattermost notification failed with exception: %s", ne), ne);
                 return null;
             }
+            listener.getLogger().println("All items");
+            for (Item a : jenkins.getAllItems()) {
+                listener.getLogger().println(a.getFullName() + " : " + a.getClass().getName());
+            }
+
+            int buildNumber = 0;
+            for (Job job: jenkins.getItemByFullName("pipeline-multibranch").getAllJobs()) {
+                RunList<Run> builds = job.getBuilds();
+                for (Run r : builds) {
+                    if(r.getResult() == null) {
+                        buildNumber = r.number;
+                    }
+                }
+            }
+
+            for (Item a : jenkins.getAllItems()) {
+                listener.getLogger().println(job.getFullName());
+            }
+
+
+            AbstractProject project = jenkins.getItemByFullName("pipeline-multibranch", AbstractProject.class);
+            AbstractBuild build = project.getBuildByNumber(buildNumber);
+
+
             MattermostNotifier.DescriptorImpl slackDesc = jenkins.getDescriptorByType(MattermostNotifier.DescriptorImpl.class);
             String team = step.endpoint != null ? step.endpoint : slackDesc.getEndpoint();
             String channel = step.channel != null ? step.channel : slackDesc.getRoom();
             String icon = step.icon != null ? step.icon: slackDesc.getIcon();
             String color = step.color != null ? step.color : "";
 
-            //placing in console log to simplify testing of retrieving values from global config or from step field; also used for tests
-            listener.getLogger().printf("Mattermost Send Pipeline step configured values from global config - connector: %s, icon: %s, channel: %s, color: %s", step.endpoint == null, step.icon == null, step.channel == null, step.color == null);
+            ActiveNotifier activeNotifier = null;
 
-            MattermostService slackService = getMattermostService(team, channel, icon);
-            boolean publishSuccess = slackService.publish(step.message, color);
-            if (!publishSuccess && step.failOnError) {
-                throw new AbortException("Mattermost notification failed. See Jenkins logs for details.");
-            } else if (!publishSuccess) {
-                listener.error("Slack notification failed. See Jenkins logs for details.");
+            Map<Descriptor<Publisher>, Publisher> map = project.getPublishersList().toMap();
+            for (Publisher publisher : map.values()) {
+                if (publisher instanceof MattermostNotifier) {
+                    activeNotifier = new ActiveNotifier((MattermostNotifier) publisher, (BuildListener)listener);
+                }
             }
+            activeNotifier.completed(build);
+//            //placing in console log to simplify testing of retrieving values from global config or from step field; also used for tests
+//            listener.getLogger().printf("Mattermost Send Pipeline step configured values from global config - connector: %s, icon: %s, channel: %s, color: %s", step.endpoint == null, step.icon == null, step.channel == null, step.color == null);
+//
+//            MattermostService slackService = getMattermostService(team, channel, icon);
+//            boolean publishSuccess = slackService.publish(step.message, color);
+//            if (!publishSuccess && step.failOnError) {
+//                throw new AbortException("Mattermost notification failed. See Jenkins logs for details.");
+//            } else if (!publishSuccess) {
+//                listener.error("Slack notification failed. See Jenkins logs for details.");
+//            }
             return null;
         }
 
